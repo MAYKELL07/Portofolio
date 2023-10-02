4d3e8a73e33dd5aa78cf969d75d79d0e0c223166
Micheal <86540880+MAYKELL07@users.noreply.github.com>
2023-10-02 16:22:39 +0000

    Complete reworkout Structure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,10 @@
-import React from 'react'; 
-import { Provider } from 'react-redux';
-import { createRoot } from 'react-dom/client';
-import { render } from '@testing-library/react';
+import React from "react"
+import { screen } from "@testing-library/react"
+import { render } from "./test-utils"
+import { App } from "./App"
 
-import App from './App';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './reducers';
-
-describe('App', () => {
-
-  test('passes store prop', () => {
-    const store = configureStore({
-      reducer: rootReducer
-    });
-  });
-
-});
+test("renders learn react link", () => {
+  render(<App />)
+  const linkElement = screen.getByText(/learn chakra/i)
+  expect(linkElement).toBeInTheDocument()
+})
