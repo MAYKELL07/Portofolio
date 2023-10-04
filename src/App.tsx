46d3ada8c617df511a4c14544635c2f05d253b5f
Micheal <86540880+MAYKELL07@users.noreply.github.com>
2023-10-04 03:22:11 +0000

    Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,11 @@ import {
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import { Header }  from "./components/header"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <Header />
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
